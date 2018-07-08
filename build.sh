9c63c7ebdc055cec8de4f641fd5c8cea2a313905
beshleman <bobbyeshleman@gmail.com>
2018-07-08 08:06:38 -0700

    Added build-chain env setup to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo umount /media/bobby/6231-3931
 make myboard_defconfig || { exit 1; }
 make u-boot.imx || { exit 1; }
 sudo dd if=u-boot.imx of=/dev/sdc bs=1k seek=1 conv=fsync | { exit 1; }
