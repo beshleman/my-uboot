ff5ba86adf699ce3ad81dea0a31b6337a7eb612f
beshleman <bobbyeshleman@gmail.com>
2018-07-08 10:12:56 -0700

    Added build chain init to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+source /opt/fsl-imx-fb/4.9.11-1.0.0/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
 sudo umount /media/bobby/6231-3931
 make myboard_defconfig || { exit 1; }
 make u-boot.imx || { exit 1; }
